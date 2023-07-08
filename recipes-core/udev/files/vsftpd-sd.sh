3ed5b484f2428bdb26d34b5da181665a3be64b2c
Hatsy Rei <43567407+HatsyRei@users.noreply.github.com>
2023-07-08 11:02:40 +0800

    Update vsftpd-sd.sh

--- recipes-core/udev/files/vsftpd-sd.sh
+++ recipes-core/udev/files/vsftpd-sd.sh
@@ -2,39 +2,18 @@
 #
 # Called from udev
 #
-# Starts/stops vsftpd.service on SD card hotplug
-
+# Mount uSD with correct permissions to FTP writable location
 
 if [ "$ACTION" = "add" ]; then
-	# Attempt to mount SD card to FTP writable location
-	ftp_uid=$(id -u ftp)
-	ftp_gid=$(id -g ftp)
-	mount -o uid=${ftp_uid},gid=${ftp_gid} /dev/mmcblk1p1 /var/lib/ftp/upload
-	retcode=$?
-	if [ $retcode -eq 0 ]; then
-		logger "Mount success."
-		# Attempt to start vsftpd.service if mount success
-		systemctl start vsftpd
-		retcode=$?
-		if [ $retcode -eq 0 ]; then
-			logger "vsftpd.service has been started by udev."
-		else
-			logger "vsftpd.service failed to start with code: " $retcode
-		fi
-	else
-		logger "Mount failed with code: " $retcode
-	fi
+    ftp_uid=$(id -u ftp)
+    ftp_gid=$(id -g ftp)
 
-elif [ "$ACTION" = "remove" ]; then
-	# Attempt to stop vsftpd.service
-	systemctl stop vsftpd
-	retcode=$?
-	if [ $retcode -eq 0 ]; then
-		logger "vsftpd.service has been stopped by udev."
-	else
-		logger "vsftpd.service failed to stop with code: " $retcode
-	fi
-else
-	# Handling edge case if something unexpected happens
-	logger "Unknown action: $ACTION triggered"
+    MOUNT="systemd-mount -o silent -o uid=${ftp_uid},gid=${ftp_gid}"
+
+    if ! $MOUNT --no-block -t auto $DEVNAME "/var/lib/ftp/upload"
+    then
+        logger "Mount failed failed!"
+    else
+        logger "Mount successful"
+    fi
 fi
